686bce120a623b09ffd22ae2c7fc004a151f09c8
Jeremi Kowalski <jeremikow@gmail.com>
2023-10-28 13:41:06 +0200

    Added MySQL integration

--- skillsprout/asgi.py
+++ skillsprout/asgi.py
@@ -13,4 +13,16 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skillsprout.settings')
 
+# Update the DATABASES setting to use MySQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'your_database_name',
+        'USER': 'your_database_user',
+        'PASSWORD': 'your_database_password',
+        'HOST': 'your_database_host',
+        'PORT': 'your_database_port',
+    }
+}
+
 application = get_asgi_application()
